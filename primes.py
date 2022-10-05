aaabc27968b99b45570bf7c11f41b3a2381e5736
cemratip <61392982+cemratip@users.noreply.github.com>
2022-10-05 17:47:36 +0100

    problem solved

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list = []
+  def is_prime(n):
+    for i in range(2,n):
+      if (n%i) == 0:
+        return False
+    return True
+  list = []
+  if (number_of_primes > 0):
+    n=2  
+    while len(list) < number_of_primes:
+      if (is_prime(n)):
+        list.append(n)
+      n+=1
     return list
+  else:
+    raise ValueError
